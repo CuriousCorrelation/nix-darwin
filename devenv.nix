6dd0c2160dd28a369124dfeb1020968d4b71573e
curiouscorrelation <curiouscorrelation@gmail.com>
2024-10-28 16:24:24 +0530

    feat: setup `devenv`

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  ...
+}: {
+  packages = [
+    pkgs.nixd
+  ];
+
+  pre-commit = {
+    hooks = {
+      black.enable = true;
+      shellcheck.enable = true;
+      alejandra.enable = true;
+      shfmt.enable = false;
+      stylua.enable = true;
+      deadnix = {
+        enable = true;
+        settings = {
+          edit = true;
+          noLambdaArg = true;
+        };
+      };
+    };
+  };
+}
