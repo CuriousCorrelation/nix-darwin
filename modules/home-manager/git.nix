b2b6e9273e76fe7483d039b391d9ff3b6029cff8
curiouscorrelation <curiouscorrelation@gmail.com>
2025-06-25 16:14:07 +0530

    home-manager/git: safer and easier worktree setup

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   home.packages = [pkgs.github-cli pkgs.git-crypt];
+
   programs.git = {
     userName = "curiouscorrelation";
     enable = true;
@@ -15,14 +16,39 @@
       pull.rebase = true;
       push.followTags = true;
       push.autoSetupRemote = true;
+      push.default = "current";
+      push.useForceIfIncludes = true;
+
+      "branch \"main\"".pushRemote = "PROTECTED_BRANCH_NO_PUSH";
+      "branch \"master\"".pushRemote = "PROTECTED_BRANCH_NO_PUSH";
+      "branch \"develop\"".pushRemote = "PROTECTED_BRANCH_NO_PUSH";
+      "branch \"dev\"".pushRemote = "PROTECTED_BRANCH_NO_PUSH";
+      "branch \"next\"".pushRemote = "PROTECTED_BRANCH_NO_PUSH";
+      "branch \"staging\"".pushRemote = "PROTECTED_BRANCH_NO_PUSH";
+      "branch \"production\"".pushRemote = "PROTECTED_BRANCH_NO_PUSH";
+      "branch \"patch\"".pushRemote = "PROTECTED_BRANCH_NO_PUSH";
+      "branch \"release\"".pushRemote = "PROTECTED_BRANCH_NO_PUSH";
+
+      receive.denyNonFastForwards = true;
     };
+
     aliases = {
       wta = ''!f() {
         branch="$1"
         upstream="''${2:-upstream/patch}"
+        [ -z "$branch" ] && { echo "Usage: git wta <branch-name> [upstream-ref]"; return 1; }
         git worktree add -b "$branch" "../$branch" "$upstream"
       }; f'';
+
+      wtd = ''!f() {
+        branch="$1"
+        [ -z "$branch" ] && { echo "Usage: git wtd <branch-name>"; return 1; }
+        [ -d "../$branch" ] && git worktree remove "../$branch" && git branch -D "$branch" 2>/dev/null || true
+      }; f'';
+
+
     };
+
     delta = {
       enable = false;
       options = {
